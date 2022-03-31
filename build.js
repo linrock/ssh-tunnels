2e0b59680394a1e777bf8b41404bd2b2be922c98
Linmiao Xu <linmiao.xu@gmail.com>
2022-03-31 13:08:15 -0400

    Use snapshot-url in build.js

--- build.js
+++ build.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
-const { JSDOM } = require('jsdom');
-const puppeteer = require('puppeteer');
-const beautify = require('js-beautify').html;
+const { Snapshot } = require('snapshot-url');
 
-const OUT_DIR = './dist';                          // output dir, relative to this file
-const HTML_OUTFILE = './dist/index.html';          // output html file, pretty-printed + cleaned
-const DELETE_NODES = 'script, link[rel=preload]';  // these html tags are removed
+const OUT_DIR = './dist';                 // output dir, relative to this file
+const HTML_OUTFILE = './dist/index.html'; // output html file, pretty-printed + cleaned
 
 if (!fs.existsSync(OUT_DIR)) {
   fs.mkdirSync(OUT_DIR);
@@ -13,26 +10,19 @@ if (!fs.existsSync(OUT_DIR)) {
 fs.copyFileSync(`./public/favicon.ico`, `${OUT_DIR}/favicon.ico`);
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  try {
-    // hack: this depends on the dev server running
-    await page.goto('http://localhost:8080', {waitUntil: 'networkidle2', timeout: 5000});
-  } catch {
-    console.error('Error: use `yarn serve` to run the dev server at http://localhost:8080')
-    await browser.close();
-    return;
-  }
-  // get the html after rendering all JS templates
-  let finalHtml = `<html>${await page.evaluate(() => document.documentElement.innerHTML)}</html>`;
+  // hack: this depends on the dev server running
+  const url = 'http://localhost:8080';
+
+  const snapshot = new Snapshot(url);
+  const dom = await snapshot.renderDOM();
+
   // remove some html tags that we don't need
-  const dom = new JSDOM(finalHtml);
+  const DELETE_NODES = 'script, link[rel=preload]';
   dom.window.document.
     querySelectorAll(DELETE_NODES).
     forEach((scriptEl) => scriptEl.parentNode.removeChild(scriptEl));
 
-  finalHtml = dom.serialize();
-  finalHtml = beautify(finalHtml, { indent_size: 2 });
+  const finalHtml = snapshot.getHtml({ prettyPrint: true });
 
   // write the final html to a file
   fs.open(HTML_OUTFILE, 'w', (err) => {
@@ -45,8 +35,6 @@ fs.copyFileSync(`./public/favicon.ico`, `${OUT_DIR}/favicon.ico`);
       } else {
         console.log(`Saved static html to dist/index.html (size: ${finalHtml.length})`);
       }
-      // exit now that we're done
-      await browser.close();
     });
   });
 })();
